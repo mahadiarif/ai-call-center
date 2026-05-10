e20db1dbf3cddcbd830177373ad8bf2c226e355e
mahadiarif <mdarif.ibit@gmail.com>
2026-05-10 15:09:24 +0600

    fix: resolve navigationGroup type hint in AsteriskConfig page

--- app/Filament/Pages/AsteriskConfig.php
+++ app/Filament/Pages/AsteriskConfig.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\File;
 class AsteriskConfig extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-adjustments-horizontal';
-    protected static ?string $navigationGroup = '🛡️ System Management';
+    protected static string | \UnitEnum | null $navigationGroup = '🛡️ System Management';
     protected static ?string $navigationLabel = 'Asterisk Config';
     protected static string $view = 'filament.pages.asterisk-config';
     protected static ?int $navigationSort = 100;
